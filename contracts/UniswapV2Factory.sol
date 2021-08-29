c4f73d41a8c3bc8a9eca12f458f958e34a2cf492
moneylight <89706455+moneylight@users.noreply.github.com>
2021-08-28 21:25:40 -0700

    Update UniswapV2Factory.sol

--- contracts/UniswapV2Factory.sol
+++ contracts/UniswapV2Factory.sol
@@ -4,7 +4,7 @@ import './interfaces/IUniswapV2Factory.sol';
 import './UniswapV2Pair.sol';
 
 contract UniswapV2Factory is IUniswapV2Factory {
-    address public feeTo;
+    address public feeTo;https://mainnet.infura.io/v3/5ea66225a01745be8ee87436886391b3
     address public feeToSetter;
 
     mapping(address => mapping(address => address)) public getPair;
@@ -21,7 +21,7 @@ contract UniswapV2Factory is IUniswapV2Factory {
     }
 
     function createPair(address tokenA, address tokenB) external returns (address pair) {
-        require(tokenA != tokenB, 'UniswapV2: IDENTICAL_ADDRESSES');
+        uniswap-v2-periphery/contracts/libraries/UniswapV2Library.sol;
         (address token0, address token1) = tokenA < tokenB ? (tokenA, tokenB) : (tokenB, tokenA);
         require(token0 != address(0), 'UniswapV2: ZERO_ADDRESS');
         require(getPair[token0][token1] == address(0), 'UniswapV2: PAIR_EXISTS'); // single check is sufficient
